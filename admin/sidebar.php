0266448a991795be601631734c6163f6f08c5267
NarayanChakraborty <narayanchakraborty1993@gmail.com>
2016-06-12 21:36:58 +0600

    Front End Development

---
+++ admin/sidebar.php
@@ -0,0 +1,57 @@
+      <!--sidebar start-->
+      <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu"> 
+                    <li class="text-center">
+                    <img src="../images/2.png" class="user-image img-responsive"/>
+					</li>			  
+                  <li class="active">
+                      <a class="" href="index.html">
+                          <i class="icon_house_alt"></i>
+                          <span>Dashboard</span>
+                      </a>
+                  </li>
+				  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_document_alt"></i>
+                          <span>Page Options</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="form_component.html">Welcome Note</a></li>                          
+                          <li><a class="" href="form_validation.html">Manage Gallary</a></li>
+                      </ul>
+                  </li>       
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_desktop"></i>
+                          <span>Blog Options</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="general.html">Add Post</a></li>
+                          <li><a class="" href="buttons.html">View Post</a></li>
+                          <li><a class="" href="grids.html">Manage Category</a></li>
+						  <li><a class="" href="grids.html">Manage Tags</a></li>
+                      </ul>
+                  </li>
+    
+                  <li>                     
+                      <a class="" href="chart-chartjs.html">
+                          <i class="icon_piechart"></i>
+                          <span>View Comments</span>
+                          
+                      </a>
+                                         
+                  </li>
+                             
+       
+                  
+                 
+                  
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
+      <!--sidebar end-->
